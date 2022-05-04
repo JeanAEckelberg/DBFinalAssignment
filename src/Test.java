166c9188492b2b449b13dd4160c8a56ef4084597
Milksoplimit <104238125+Milksoplimit@users.noreply.github.com>
2022-05-04 15:33:52 -0500

    Continuing Construction
    Added static and nonstatic methods for getting the number of questions and topics in a test

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 /**
- *
+ * A class representing the Test entity in the Discrete Mathematics 
+ * study tool application
  * @author Greg
+ * @version May 4 2022
  */
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -64,6 +66,14 @@ public class Test {
         questions = pullQuestions(c);
         topics = pullTopics(c);
         
+    }
+    
+    /**
+    * Private helper method to check and see if the has permissions to modify/remove a test
+    */
+    private boolean validatePerms(Connection c, int id) throws SQLException {
+        int permLvl = User.getPermissionLevel(c, id);
+        return permLvl > 1 || id == creatorID;
     }
     
     /**
@@ -157,5 +167,117 @@ public class Test {
         return toReturn;
     }
     
+    /**
+    * Method to get the number of questions associated with this test object
+    */
+    public int getNumQuestions(){
+        return questions.size();
+    }
     
+    /**
+     * Static method that takes a connection and a testID and returns the number of 
+     * questions in that test
+     * @param c Connection to the database
+     * @param testID identifier for the test in the db
+     * @return number of questions in the test passed
+     */
+    public static int getNumQuestions(Connection c, int testID){
+        String quereyString = "Select count(*) from questionInTest where testID = ?";
+        PreparedStatement quereyStmt;
+        ResultSet set;
+        
+        try{
+            quereyStmt = c.prepareStatement(quereyString);
+        }
+        catch (SQLException e){
+            System.err.println("Can't prep statement in getNumQuestions of Test class.");
+            System.exit(1);
+            return -1;
+        }
+        
+        try{
+            quereyStmt.setInt(1, testID);
+        } 
+        catch (SQLException e) {
+            System.err.println("Can't set testID in getNumQuestions of Test class");
+            System.exit(1);
+            return -1;
+        }
+        
+        try{
+            set = quereyStmt.executeQuery();
+            if (!set.next()) throw new SQLException();
+            int toReturn = set.getInt(1);
+            set.close();
+            return toReturn;
+            
+        }
+        catch (SQLException e){
+            System.err.println("Can't execute query in getNumQuestions of Test class");
+            System.exit(1);
+            return -1;
+        }
+    }
+    
+    /**
+    * Method to get the number of topics associated with this test object
+    */
+    public int getNumTopics(){
+        return topics.size();
+    }
+    
+    /**
+     * Static method that takes a connection and a testID and returns the number of 
+     * topics in that test
+     * @param c Connection to the database
+     * @param topicID identifier for the test in the db
+     * @return number of topics in the test passed
+     */
+    public static int getNumTopics(Connection c, int topicID){
+        String quereyString = "Select count(*) from topicInTest where testID = ?";
+        PreparedStatement quereyStmt;
+        ResultSet set;
+        
+        try{
+            quereyStmt = c.prepareStatement(quereyString);
+        }
+        catch (SQLException e){
+            System.err.println("Can't prep statement in getNumTopics of Test class.");
+            System.exit(1);
+            return -1;
+        }
+        
+        try{
+            quereyStmt.setInt(1, topicID);
+        } 
+        catch (SQLException e) {
+            System.err.println("Can't set testID in getNumTopics of Test class");
+            System.exit(1);
+            return -1;
+        }
+        
+        try{
+            set = quereyStmt.executeQuery();
+            if (!set.next()) throw new SQLException();
+            int toReturn = set.getInt(1);
+            set.close();
+            return toReturn;
+            
+        }
+        catch (SQLException e){
+            System.err.println("Can't execute query in getNumTopics of Test class");
+            System.exit(1);
+            return -1;
+        }
+    }
+    
+    /**
+     * A static method to insert a Test into the database
+     * @param c Connection to the database
+     * @param questions An array of questions already in the database
+     * @param creatorID identifier of the user who made the test
+     */
+    public static void createTest(Connection c, Question[] questions, int creatorID){
+        // Under construction
+    }
 }
