f7fd22ba5e51082fb684e38340443691b2d33d9b
Milksoplimit <104238125+Milksoplimit@users.noreply.github.com>
2022-05-06 11:58:17 -0500

    Continued Progress
    Finished createTest and addQuestion and addTopic

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 /**
  * A class representing the Test entity in the Discrete Mathematics 
  * study tool application
- * @author Greg
+ * @author Gregory Beaucalir
  * @version May 4 2022
  */
 import java.sql.Connection;
@@ -23,8 +23,9 @@ public class Test {
      * object with the result set
      * @param c Connection to the database
      * @param testID The unique identifier for each test in the database as an integer
+     * @throws java.sql.SQLException
      */
-    public Test(Connection c, int testID) {
+    public Test(Connection c, int testID) throws SQLException {
         
         questions = new ArrayList<>();
         topics = new ArrayList<>();
@@ -37,17 +38,13 @@ public class Test {
             quereyStatement = c.prepareStatement(quereyString);
         }
         catch (SQLException e){
-            System.err.println("Can't prep statement in Test class.");
-            System.exit(1);
-            return;
+            throw new SQLException("Can't prep statement in Test class.");
         }
         
         try{
             quereyStatement.setInt(1, testID);
         } catch (SQLException e) {
-            System.err.println("Can't set testID");
-            System.exit(1);
-            return;
+            throw new SQLException("Can't set testID");
         }
         
         try{
@@ -58,14 +55,12 @@ public class Test {
             set.close();
         }
         catch (SQLException e){
-            System.err.println("Can't execute query in Test class constructor.");
-            System.exit(1);
-            return;
+            throw new SQLException("Can't execute query in Test class constructor.");
         }
         
-        questions = pullQuestions(c);
-        topics = pullTopics(c);
-        
+        this.c = c;
+        questions = pullQuestions();
+        topics = pullTopics();
     }
     
     /**
@@ -82,7 +77,7 @@ public class Test {
      * @param c Connection to database information is pulled from
      * @return The arrayList of questions associated with the test
      */
-    private ArrayList<Question> pullQuestions(Connection c){
+    private ArrayList<Question> pullQuestions() throws SQLException {
         String quereyString = "Select questionID from questionInTest where testID = ?";
         PreparedStatement quereyStmt;
         ResultSet set;
@@ -92,18 +87,14 @@ public class Test {
             quereyStmt = c.prepareStatement(quereyString);
         }
         catch (SQLException e){
-            System.err.println("Can't prep statement in pullQuestions of Test class.");
-            System.exit(1);
-            return null;
+            throw new SQLException("Can't prep statement in pullQuestions of Test class.");
         }
         
         try{
             quereyStmt.setInt(1, testID);
         } 
         catch (SQLException e) {
-            System.err.println("Can't set testID in pullQuestions of Test class");
-            System.exit(1);
-            return null;
+            throw new SQLException("Can't set testID in pullQuestions of Test class");
         }
         
         try{
@@ -114,9 +105,7 @@ public class Test {
             set.close();
         } 
         catch (SQLException e){
-            System.err.println("Can't execute query in pullQuestions of Test class");
-            System.exit(1);
-            return null;
+            throw new SQLException("Can't execute query in pullQuestions of Test class");
         }
         
         return toReturn;
@@ -127,7 +116,7 @@ public class Test {
      * @param c Connection to the database data is pulled from
      * @return ArrayList of topics that are associated with this test
      */
-    private ArrayList<Topic> pullTopics(Connection c){
+    private ArrayList<Topic> pullTopics() throws SQLException {
         String quereyString = "Select topicID from topicInTest where testID = ?";
         PreparedStatement quereyStmt;
         ResultSet set;
@@ -137,18 +126,14 @@ public class Test {
             quereyStmt = c.prepareStatement(quereyString);
         }
         catch (SQLException e){
-            System.err.println("Can't prep statement in pullTopics of Test class.");
-            System.exit(1);
-            return null;
+            throw new SQLException("Can't prep statement in pullTopics of Test class.");
         }
         
         try{
             quereyStmt.setInt(1, testID);
         } 
         catch (SQLException e) {
-            System.err.println("Can't set testID in pullTopics of Test class");
-            System.exit(1);
-            return null;
+            throw new SQLException("Can't set testID in pullTopics of Test class");
         }
         
         try{
@@ -159,9 +144,7 @@ public class Test {
             set.close();
         } 
         catch (SQLException e){
-            System.err.println("Can't execute query in pullTopics of Test class");
-            System.exit(1);
-            return null;
+            throw new SQLException("Can't execute query in pullTopics of Test class");
         }
         
         return toReturn;
@@ -180,8 +163,9 @@ public class Test {
      * @param c Connection to the database
      * @param testID identifier for the test in the db
      * @return number of questions in the test passed
+     * @throws java.sql.SQLException
      */
-    public static int getNumQuestions(Connection c, int testID){
+    public static int getNumQuestions(Connection c, int testID) throws SQLException {
         String quereyString = "Select count(*) from questionInTest where testID = ?";
         PreparedStatement quereyStmt;
         ResultSet set;
@@ -190,18 +174,14 @@ public class Test {
             quereyStmt = c.prepareStatement(quereyString);
         }
         catch (SQLException e){
-            System.err.println("Can't prep statement in getNumQuestions of Test class.");
-            System.exit(1);
-            return -1;
+            throw new SQLException("Can't prep statement in getNumQuestions of Test class.");
         }
         
         try{
             quereyStmt.setInt(1, testID);
         } 
         catch (SQLException e) {
-            System.err.println("Can't set testID in getNumQuestions of Test class");
-            System.exit(1);
-            return -1;
+            throw new SQLException("Can't set testID in getNumQuestions of Test class");
         }
         
         try{
@@ -213,9 +193,7 @@ public class Test {
             
         }
         catch (SQLException e){
-            System.err.println("Can't execute query in getNumQuestions of Test class");
-            System.exit(1);
-            return -1;
+            throw new SQLException("Can't execute query in getNumQuestions of Test class");
         }
     }
     
@@ -230,10 +208,11 @@ public class Test {
      * Static method that takes a connection and a testID and returns the number of 
      * topics in that test
      * @param c Connection to the database
-     * @param topicID identifier for the test in the db
+     * @param testID identifier for the test in the db
      * @return number of topics in the test passed
+     * @throws java.sql.SQLException
      */
-    public static int getNumTopics(Connection c, int topicID){
+    public static int getNumTopics(Connection c, int testID) throws SQLException{
         String quereyString = "Select count(*) from topicInTest where testID = ?";
         PreparedStatement quereyStmt;
         ResultSet set;
@@ -242,18 +221,14 @@ public class Test {
             quereyStmt = c.prepareStatement(quereyString);
         }
         catch (SQLException e){
-            System.err.println("Can't prep statement in getNumTopics of Test class.");
-            System.exit(1);
-            return -1;
+            throw new SQLException("Can't prep statement in getNumTopics of Test class.");
         }
         
         try{
-            quereyStmt.setInt(1, topicID);
+            quereyStmt.setInt(1, testID);
         } 
         catch (SQLException e) {
-            System.err.println("Can't set testID in getNumTopics of Test class");
-            System.exit(1);
-            return -1;
+            throw new SQLException("Can't set testID in getNumTopics of Test class");
         }
         
         try{
@@ -265,19 +240,180 @@ public class Test {
             
         }
         catch (SQLException e){
-            System.err.println("Can't execute query in getNumTopics of Test class");
-            System.exit(1);
-            return -1;
+            throw new SQLException("Can't execute query in getNumTopics of Test class");
         }
     }
     
     /**
-     * A static method to insert a Test into the database
+     * A static method to insert a Test and it's questions into the database that 
+     * uses a transaction to ensure that there are no orphaned questions or 
+     * other problems
      * @param c Connection to the database
      * @param questions An array of questions already in the database
      * @param creatorID identifier of the user who made the test
+     * @throws java.sql.SQLException
      */
-    public static void createTest(Connection c, Question[] questions, int creatorID){
-        // Under construction
+    public static void createTest(Connection c, Question[] questions, 
+            int creatorID) throws SQLException {
+        
+        // Begin Transaction
+        try{
+            c.setAutoCommit(false); // set to false in order to make a transaction
+        
+            // get the next testID for the linking table inserts
+            String nextIDString = "select count(*) from test";
+            PreparedStatement nextIDStmt;
+            int nextID  = -1;
+            ResultSet set;
+
+            try{
+                nextIDStmt = c.prepareStatement(nextIDString);
+            }
+            catch (SQLException e){
+                throw new SQLException("Can't prep statemtent to get the next testID in createTest of Test class");
+            }
+
+            try{
+                set = nextIDStmt.executeQuery();
+                if (!set.next()) throw new SQLException();
+                nextID = set.getInt(1);
+                set.close();
+            }
+            catch(SQLException e){
+                throw new SQLException("Can't execute statemtent to get the next testID in createTest of Test class");
+            }
+
+            // the test insertion
+            String insertTestString = "insert into test (numberOfQuestions, creator) values (?, ?)";
+            PreparedStatement insertTestStmt;
+
+            try{
+                insertTestStmt = c.prepareStatement(insertTestString);
+                insertTestStmt.setInt(1, questions.length);
+                insertTestStmt.setInt(2, creatorID);
+            }
+            catch (SQLException e){
+                throw new SQLException("Can't prep test insert statement in createTest of Test class.");
+            }
+
+            try{
+                insertTestStmt.executeUpdate();
+                insertTestStmt.close();
+            }
+            catch (SQLException e){
+                throw new SQLException("Can't execute test insert statement in createTest of Test class.");
+            }
+            
+            // the question insertion into the linking table
+            String insertQuestionsString = "insert into questionInTest (questionID, testID) values (?, " + nextID + " )";
+            PreparedStatement insertQuestionsStmt;
+            
+            for (Question q : questions){
+                try{
+                    insertQuestionsStmt = c.prepareStatement(insertQuestionsString);
+                    insertQuestionsStmt.setInt(1, q.getID());
+                    insertQuestionsStmt.executeUpdate();
+                }
+                catch (SQLException e){
+                    throw new SQLException("Problem adding Questions to the Test in createTest of Test class");
+                }
+            }
+            
+            c.commit();
+        
+        } // end transaction
+        
+        catch (SQLException exc){
+            try{
+                c.rollback(); // rollback on failure
+            }
+            catch (SQLException roll){
+                throw new SQLException("Problem rolling back transaction from error: " + exc.getMessage());
+            }
+            
+        }
+        finally{ // always do this
+            c.setAutoCommit(true); // set back to true to prevent problems elsewhere in application
+        }
+        
     }
+    
+    /**
+     * Method to add a single question to an existing test
+     * @param question question to be added
+     * @param userID id of user attempting to modify the test
+     * @throws SQLException 
+     */
+    public void addQuestionToTest(Question question, int userID) throws SQLException{
+        
+        if (!validatePerms(c, userID)) return;
+        
+        String insertString = "insert into questionInTest (questionID, testID) values (?, " + testID + " )";
+        PreparedStatement insertStmt;
+        
+        
+        try {
+            insertStmt = c.prepareStatement(insertString);
+        }
+        catch (SQLException e){
+            throw new SQLException("Can't prep statement in addQuestionToTest in Test class");
+        }
+        
+        try{
+            insertStmt.setInt(1, question.getID());
+        }
+        catch (SQLException e) {
+            throw new SQLException("Can't set questionID in addQuestionToTest in Test class");
+        }
+        
+        try{
+            insertStmt.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new SQLException("Can't execute update in addQuestionToTest in Test class");
+        }
+        
+        questions.add(question);
+        
+    }
+    
+    /**
+     * Method to add a single Topic to an existing test
+     * @param topic topic to be added
+     * @param userID id of user attempting to modify the test
+     * @throws SQLException 
+     */
+    public void addTopicToTest (Topic topic, int userID) throws SQLException {
+        
+        if (!validatePerms(c, userID)) return;
+        
+        String insertString = "insert into topicInTest (topicID, testID) values (?, " + testID + " )";
+        PreparedStatement insertStmt;
+        
+        
+        try {
+            insertStmt = c.prepareStatement(insertString);
+        }
+        catch (SQLException e){
+            throw new SQLException("Can't prep statement in addTopicToTest in Test class");
+        }
+        
+        try{
+            insertStmt.setInt(1, topic.getID());
+        }
+        catch (SQLException e) {
+            throw new SQLException("Can't set questionID in addTopicToTest in Test class");
+        }
+        
+        try{
+            insertStmt.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new SQLException("Can't execute update in addTopicToTest in Test class");
+        }
+        
+        topics.add(topic);
+    }
+    
+    
 }
