04513bb5b5a0a3c20454811b8b21f8a8a75c5e41
JeanAEckelberg <fyrelynx11@gmail.com>
2022-05-10 05:41:59 -0500

    Update Window.java

--- src/Window.java
+++ src/Window.java
@@ -651,6 +651,13 @@ public class Window {
         
         
         for(int i = 0; i < results.length; i++){
+            if(tests.isEmpty()) {
+                results[i] = new JButton("No Results Found");
+                results[i].setBounds(frame.getWidth()/2 - 150, 200+(i*60), 300, 50);
+                resultsPage.add(results[i]);
+                continue;
+            }
+            
             Test currentTest = tests.get(Math.min(Math.max(0,tests.size()-1), cursor+i));
             results[i] = new JButton(currentTest.getTestName());
             take[i] = new JButton("Take Test");
@@ -803,6 +810,13 @@ public class Window {
         
         
         for(int i = 0; i < results.length; i++){
+            if(topics.isEmpty()) {
+                results[i] = new JButton("No Results Found");
+                results[i].setBounds(frame.getWidth()/2 - 150, 200+(i*60), 300, 50);
+                resultsPage.add(results[i]);
+                continue;
+            }
+            
             Topic currentTopic = topics.get(Math.min(Math.max(0,topics.size()-1), cursor+i));
             results[i] = new JButton(currentTopic.getName());
             results[i].setBounds(frame.getWidth()/2 - 150, 200+(i*60), 300, 50);
@@ -950,6 +964,13 @@ public class Window {
         
         
         for(int i = 0; i < results.length; i++){
+            if(questions.isEmpty()) {
+                results[i] = new JButton("No Results Found");
+                results[i].setBounds(resultsPage.getWidth()/2 - 150, 200+(i*60), 300, 50);
+                resultsPage.add(results[i]);
+                continue;
+            }
+            
             Question currentQuestion = questions.get(Math.min(Math.max(0,questions.size()-1), cursor+i));
             results[i] = new JButton(currentQuestion.getText());
             results[i].setBounds(frame.getWidth()/2 - 150, 200+(i*60), 300, 50);
